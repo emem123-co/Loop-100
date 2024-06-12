5def8c5c695154b3f98fa6526a0cdb42c59f85dc
Emily Morgan <emorgan921@gmail.com>
2024-06-12 16:12:16 -0400

    Add project files.

---
+++ Loop 100/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Loop_100;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            int sum = 0;
+
+            for (var idx = 1; idx < 100; idx++)
+            {
+                if (idx % 5 == 0 || idx % 7 == 0)
+                {
+                    sum = sum + idx;
+                }
+            }
+           
+            Console.WriteLine(sum);
+        }
+    }
+}
